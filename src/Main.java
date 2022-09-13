9b19befc6bb20530e2d5bd6a53ae5d0d748b1572
하정빈 <@DESKTOP-OQ46K1A>
2022-09-13 22:20:23 +0900

    숨은 번호 맞추기 게임

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,62 @@
 import java.util.Scanner;
+import java.util.Random;
 public class Main{
 	public static void main(String[] args)
 	{
-		double score[][] = {{3.3, 3.4},
-							{3.5, 3.6},
-							{3.7, 4.0},
-							{4.1, 4.2}};
+			Scanner in = new Scanner(System.in);
+			Random r = new Random();
+			int count=1;
+			int k = r.nextInt(100); // 0~99까지의 임의의 정수 생성
+			System.out.println("Up & Down게임입니다. 숨겨진 수를 맞추어 보세요");
+			System.out.println("0-99");
+			int max = 99;
+			int min =0;
+			while(true)
+			{	
 		
-		double sum=0;
+			System.out.print(count+">> ");
+			
+			
+			
+				int number = in.nextInt(); // 정수 입력 받음
+			
+			if(number<k)
+			{
+				System.out.println("더 높게");
+				System.out.println(number+"-"+max);
+				count++;
+				max = k;
+				min = number;
+			}
+			else if(number>k)
+			{
+				System.out.println("더 낮게");
+				System.out.println(min+"-"+number);
+				count++;
+				max = number;
+				min = k;
+			}
+			if(number==k)
+			{
+				System.out.println("맞췄습니다");
+				System.out.println("다시하시겠습니까(y/n)>>");
+				if(in.next().equals("y"))
+				{	System.out.println("Up & Down게임입니다. 숨겨진 수를 맞추어 보세요");
+					System.out.println("0-99");
+					count =1;
+					max =99;
+					min =0;
+					 continue;
+				}
+				else if(in.next().equals("n"))
+				{
+					 break;
+				}
+			}
+			
+			
 		
-		for(int year =0; year<score.length; year++)
-			for(int term=0; term<score[year].length; term++)
-				sum += score[year][term];
-		
-		int n= score.length;
-		int m = score[0].length;
-		System.out.println("4년 전체 평점 평균은" + sum/(n*m));
-				
-		
-		
-		}
+		}	
+			
 	}
-	
+}
